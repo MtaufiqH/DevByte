a0c40c7b42d9461b9ccbd6dbe8531842cedcb500
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-08-21 22:28:41 +0800

    implementing repository

--- app/src/main/java/app/taufiq/devbyte/repository/VideoRepository.kt
+++ app/src/main/java/app/taufiq/devbyte/repository/VideoRepository.kt
@@ -1,8 +1,43 @@
 package app.taufiq.devbyte.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import app.taufiq.devbyte.database.VideoDatabase
+import app.taufiq.devbyte.database.asDomainModel
+import app.taufiq.devbyte.domain.DevbyteVideos
+import app.taufiq.devbyte.network.DevByteNetwork
+import app.taufiq.devbyte.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 /**
  * Created By Taufiq on 8/18/2020.
  *
  */
-class VideoRepository {
+
+
+/**
+ * Repository for fetching devbyte videos from the network and storing them on disk
+ */
+class VideoRepository(private val database: VideoDatabase) {
+
+    /**
+     * Refresh the videos stored in the offline cache.
+     *
+     * This function uses the IO dispatcher to ensure the database insert database operation
+     * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext` this
+     * function is now safe to call from any thread including the Main thread.
+     *
+     */
+    suspend fun refreshVideo() {
+        withContext(Dispatchers.IO) {
+            val playlist = DevByteNetwork.devbytes.getPlaylist().await()
+
+            database.videoDao.insertAll(playlist.asDatabaseModel())
+        }
+    }
+
+    val videos: LiveData<List<DevbyteVideos>> = Transformations.map(database.videoDao.getVideo()){
+        it.asDomainModel()
+    }
 }
